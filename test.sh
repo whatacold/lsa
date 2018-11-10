045fadc9e6a359fca6d337471ed12337e1b2e6dd
whatacold <whatacold@gmail.com>
2018-11-10 10:24:34 +0800

    Trivial test.

--- test.sh
+++ test.sh
@@ -5,3 +5,6 @@ PY=python3
 $PY -u lsw.py $PY test/echo.py <<EOF
 HELLO WORLD, some text.
 EOF
+
+# relay output of subprocess
+$PY -u lsw.py echo "hello world"
--- test/ls.py
+++ test/ls.py
@@ -5,17 +5,19 @@
 
 import sys
 import time
+import os
 
 responses = [
-    "Content-Length: 12\r\n\r\nhello world.",
-    'Content-Length: 1142\r\n\r\n{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"processId":10596,"rootPath":"/home/hgw/test/eglot-gbk-demo/","rootUri":"file:///home/hgw/test/eglot-gbk-demo/","initializationOptions":null,"capabilities":{"workspace":{"applyEdit":true,"executeCommand":{"dynamicRegistration":false},"workspaceEdit":{"documentChanges":false},"didChangeWatchedFiles":{"dynamicRegistration":true},"symbol":{"dynamicRegistration":false}},"textDocument":{"synchronization":{"dynamicRegistration":false,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":false,"completionItem":{"snippetSupport":true}},"hover":{"dynamicRegistration":false},"signatureHelp":{"dynamicRegistration":false},"references":{"dynamicRegistration":false},"definition":{"dynamicRegistration":false},"documentSymbol":{"dynamicRegistration":false},"documentHighlight":{"dynamicRegistration":false},"codeAction":{"dynamicRegistration":false},"formatting":{"dynamicRegistration":false},"rangeFormatting":{"dynamicRegistration":false},"rename":{"dynamicRegistration":false},"publishDiagnostics":{"relatedInformation":false}},"experimental":null}}}'
-    u'Content-Length: 36\r\n\r\n{"content":"你好","charset":"utf-8"}',
+    "hello world.",
+    '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"processId":10596,"rootPath":"/home/hgw/test/eglot-gbk-demo/","rootUri":"file:///home/hgw/test/eglot-gbk-demo/","initializationOptions":null,"capabilities":{"workspace":{"applyEdit":true,"executeCommand":{"dynamicRegistration":false},"workspaceEdit":{"documentChanges":false},"didChangeWatchedFiles":{"dynamicRegistration":true},"symbol":{"dynamicRegistration":false}},"textDocument":{"synchronization":{"dynamicRegistration":false,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":false,"completionItem":{"snippetSupport":true}},"hover":{"dynamicRegistration":false},"signatureHelp":{"dynamicRegistration":false},"references":{"dynamicRegistration":false},"definition":{"dynamicRegistration":false},"documentSymbol":{"dynamicRegistration":false},"documentHighlight":{"dynamicRegistration":false},"codeAction":{"dynamicRegistration":false},"formatting":{"dynamicRegistration":false},"rangeFormatting":{"dynamicRegistration":false},"rename":{"dynamicRegistration":false},"publishDiagnostics":{"relatedInformation":false}},"experimental":null}}}',
+    u'{"content":"你好","charset":"utf-8"}',
 ]
 
-utf8 = u'Content-Length: 34\r\n\r\n{"content":"你好","charset":"gbk"}'
-gbk = utf8.encode('gbk')
-responses.append(gbk)
+sys_stdout = os.fdopen(sys.stdout.fileno(), "wb")
 
 for response in responses:
-    sys.stdout.write(response)
-    sys.stdout.flush()
+    encoding = sys.argv[1]
+    response_byte = response.encode(encoding)
+    sys_stdout.write("Content-Length: {}\r\n\r\n".format(len(response_byte)).encode(encoding))
+    sys_stdout.write(response_byte)
+    sys_stdout.flush()
